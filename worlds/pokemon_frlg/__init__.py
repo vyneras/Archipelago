4191f9fa5c5cf4fbd15954c9d9d1ad6dfe22a12c
RhenaudTheLukark <rhen33390@gmail.com>
2026-02-17 02:58:43 +0000

    Update Pokemon Gen 3 Adjuster constants

--- worlds/pokemon_frlg/__init__.py
+++ worlds/pokemon_frlg/__init__.py
@@ -43,8 +43,9 @@ from .util import int_to_bool_array, HM_TO_COMPATIBILITY_ID
 # Try adding the Pokemon Gen 3 Adjuster
 try:
     from worlds._pokemon_gen3_adjuster import __init__
-except:
-    pass
+except Exception as e:
+    message = getattr(e, "message") if hasattr(e, "message") else str(e)
+    logging.warning(f"Could not load the Pokemon Gen 3 Adjuster: {message}")
 
 class PokemonFRLGWebWorld(WebWorld):
     """
